6c8236f5d033e44094c40429baf6d4aa8d4a9783
Logan Praz <158003246+Praz18@users.noreply.github.com>
2024-02-10 20:42:00 -0600

    Update rolls.js
    fixed rolls

--- rolls.js
+++ rolls.js
@@ -1,9 +1,11 @@
 const button = document.getElementById("button");
 const roll1 = document.getElementById("roll1");
 const roll2 = document.getElementById("roll2");
+let num1;
+let num2;
 button.onclick = function(){
 	num1 = Math.floor(Math.random() * 6) + 1;
 	num2 = Math.floor(Math.random() * 6) + 1;
-	roll1.textContent = num1
-	roll2.textContent = num2
+	roll1.textContent = num1;
+	roll2.textContent = num2;
 }
