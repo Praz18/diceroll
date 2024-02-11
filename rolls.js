c0435dec9a9cd03429e3bcea6e72cefc9242560c
Logan Praz <158003246+Praz18@users.noreply.github.com>
2024-02-10 20:37:52 -0600

    Add files via upload

---
+++ rolls.js
@@ -0,0 +1,9 @@
+const button = document.getElementById("button");
+const roll1 = document.getElementById("roll1");
+const roll2 = document.getElementById("roll2");
+button.onclick = function(){
+	num1 = Math.floor(Math.random() * 6) + 1;
+	num2 = Math.floor(Math.random() * 6) + 1;
+	roll1.textContent = num1
+	roll2.textContent = num2
+}
