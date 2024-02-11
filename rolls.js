1f7914d4ec608472ecd1a886e3f4110ad8f709d3
Logan Praz <158003246+Praz18@users.noreply.github.com>
2024-02-10 20:49:42 -0600

    Update rolls.js

--- rolls.js
+++ rolls.js
@@ -1,11 +1,8 @@
-const button = document.getElementById("button");
-const roll1 = document.getElementById("roll1").innerHTML;
-const roll2 = document.getElementById("roll2").innerHTML;
 let num1;
 let num2;
-button.onclick = function(){
+document.getElementById("button").onclick = function(){
 	num1 = Math.floor(Math.random() * 6) + 1;
 	num2 = Math.floor(Math.random() * 6) + 1;
-	roll1.textContent = num1;
-	roll2.textContent = num2;
+	document.getElementById("roll1").innerHTML = num1;
+	document.getElementById("roll2").innerHTML = num2;
 }
