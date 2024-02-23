bb0e6d1e8dcde5d9d745d9900593669126e6bb90
Logan Praz <158003246+Praz18@users.noreply.github.com>
2024-02-23 11:46:05 -0600

    Update rolls.js

--- rolls.js
+++ rolls.js
@@ -3,8 +3,8 @@ let num2;
 document.getElementById("rollClick").onclick = function(){
 	num1 = Math.floor(Math.random() * 6) + 1;
 	num2 = Math.floor(Math.random() * 6) + 1;
-	document.getElementById("roll2").innerHTML = num1;
-	document.getElementById("image1").innerHTML = num1 + ".png";
-	document.getElementById("roll2").innerHTML = num2;
-	document.getElementById("image2").innerHTML = num2 + ".png";
+	document.getElementById("roll2").innerHTML = num1.toString();
+	document.getElementById("image1").innerHTML = num1.toString() + ".png";
+	document.getElementById("roll2").innerHTML = num2.toString();
+	document.getElementById("image2").innerHTML = num2.toString() + ".png";
 }
